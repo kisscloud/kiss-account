30e7bb87fe7cd593eb955cb7e076066d079262d5
qianrongli <qianrongli@uchain.one>
2018-10-12 16:32:28 +0800

    add filterChain

--- server/src/main/java/com/kiss/account/Filter/ResponseFilter.java
+++ server/src/main/java/com/kiss/account/Filter/ResponseFilter.java
@@ -2,40 +2,27 @@ package com.kiss.account.Filter;
 
 import com.alibaba.fastjson.JSONObject;
 import com.kiss.account.out.Message;
-import org.apache.catalina.core.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.stereotype.Component;
-import org.springframework.util.StreamUtils;
 import org.springframework.util.StringUtils;
 
-import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.charset.Charset;
 
-@Component
-@WebFilter(filterName = "responseFilter",urlPatterns = "/*")
-public class ResponseFilter implements Filter {
+public class ResponseFilter implements InnerFilter {
 
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+    private ResponseWrapper responseWrapper;
 
+    public ResponseFilter (ResponseWrapper responseWrapper) {
+        this.responseWrapper = responseWrapper;
     }
-
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        ResponseWrapper responseWrapper = new ResponseWrapper((HttpServletResponse)response);
-        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-        chain.doFilter(request,responseWrapper);
-
+    public void doFilter(HttpServletRequest request, HttpServletResponse response,InnerFilterChain filterChain) {
+        filterChain.doFilter(request,response,filterChain);
         byte[] bytes = responseWrapper.getBytes();
-        String responseMsg = new String(bytes,"utf-8");
         try {
+            String responseMsg = new String(bytes,"utf-8");
             JSONObject jsonObject = JSONObject.parseObject(responseMsg);
-            String lang = httpServletRequest.getHeader("X-LANGUAGE");
+            String lang = request.getHeader("X-LANGUAGE");
             if(!StringUtils.isEmpty(lang) && !StringUtils.isEmpty(jsonObject.getInteger("code")) && StringUtils.isEmpty(jsonObject.getString("message"))) {
                 String message = Message.getMessage(lang,jsonObject.getInteger("code"));
                 jsonObject.put("message",message);
@@ -44,12 +31,11 @@ public class ResponseFilter implements Filter {
         } catch (Exception e) {
 
         } finally {
-            response.getOutputStream().write(bytes);
+            try {
+                response.getOutputStream().write(bytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-    }
-
-    @Override
-    public void destroy() {
-
     }
 }
