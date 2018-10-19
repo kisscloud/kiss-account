eafe96574611d8fa76b08262bc3689b7dca984f8
qianrongli <qianrongli@uchain.one>
2018-10-19 18:24:22 +0800

    modify lang

--- server/src/main/java/com/kiss/account/utils/DbEnumsUtil.java
+++ server/src/main/java/com/kiss/account/utils/DbEnumsUtil.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class DbEnumsUtil {
     public static Map<String,Map<String,String>> dbEnumsDao = new HashMap<>();
 
-    public static String getStatusValue(String key,String option) {
+    public static String getValue(String key,String option) {
         String lang = ApplicationUtil.getHttpServletRequest().getHeader("X-LANGUAGE");
         return dbEnumsDao.get(lang == null ? "zh-CN" : lang).get(key + option);
     }
