cc05634015600de462febdfc6bc29a07b30a4e78
qianrongli <qianrongli@uchain.one>
2018-10-29 21:08:11 +0800

    client auth no filter

--- server/src/main/java/com/kiss/account/filter/AccountFilter.java
+++ server/src/main/java/com/kiss/account/filter/AccountFilter.java
@@ -35,7 +35,7 @@ public class AccountFilter implements Filter {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         InnerFilterChain preFilterChain = new InnerFilterChain();
 
-        if (!httpServletRequest.getRequestURI().contains("/login")) {
+        if (!httpServletRequest.getRequestURI().contains("/login")|| !httpServletRequest.getRequestURI().contains("/client/authorization")) {
             GuestFilter userInfoFilter = new GuestFilter();
             preFilterChain.addFilter(userInfoFilter);
         }
