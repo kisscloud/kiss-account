20476fa7ab1e63191a631614994e9f03a1b1e161
qianrongli <qianrongli@uchain.one>
2018-10-29 09:47:14 +0800

    modify log

--- server/src/main/java/com/kiss/account/KissAccountApplication.java
+++ server/src/main/java/com/kiss/account/KissAccountApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @EnableAutoConfiguration
-@EnableEurekaClient
 public class KissAccountApplication {
 
     public static void main(String[] args) {
